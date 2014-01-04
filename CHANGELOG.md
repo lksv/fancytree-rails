430aa5a2ad6ff007230f61d24a5ba746b468a1e6
Lukas Svoboda <lukas.svoboda@gmail.com>
2014-01-04 17:01:13 +0100

    chore(release): v0.0.2

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+## v0.0.2
+
+* fix icons paths
+* added awesome extension (it is in separate js file)
+
+## v0.0.1
+
+* inital release
--- lib/fancytree/rails/version.rb
+++ lib/fancytree/rails/version.rb
@@ -1,6 +1,6 @@
 module Fancytree
   module Rails
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
     FANCYTREE_VERSION="v2.0.0-5"
   end
 end
